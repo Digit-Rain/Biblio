6888a624370ca952e0f1dd69848d722e2b4859ed
davdevo <davdroid67@gmail.com>
2017-03-06 09:46:22 +0100

    Exemplaire

--- Biblio_UML/src/metier/Exemplaire.java
+++ Biblio_UML/src/metier/Exemplaire.java
@@ -1,28 +1,99 @@
 package metier;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.GregorianCalendar;
+import dao.ExemplairesDAO;
 
-
-public class Exemplaire 
-{
-	private int idExemplaire = 0 ;
-	private GregorianCalendar dateAchat = new GregorianCalendar(2017,0,1);
-	private EnumStatusExemplaire status = EnumStatusExemplaire.DISPONIBLE;
-	private String isbn = "Non renseignÃ©";
-		
+public class Exemplaire {
+	private int idExemplaire;
+	private Date dateAchat;
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	private EnumStatusExemplaire status;
+	private String isbn = "isbn inconnu";
+//	ExemplaireDao exDao = new ExemplaireDao();
+	
+	public Exemplaire(int idExemplaire, Date dateAchat, EnumStatusExemplaire status, String isbn) {
+		this.setIdExemplaire(idExemplaire);
+		this.setDateAchat(dateAchat);
+		this.setStatus(status);
+		this.setIsbn(isbn);
+	//	exDao.addExemplaire(this); // Chaque Exemplaire crée est ajouté à la DataBase virtuelle ExemplaireDB.
+	}
 	
 	public Exemplaire() {}
 	
-	public Exemplaire ( int idExemplaire, GregorianCalendar dateAchat, EnumStatusExemplaire status, String isbn)
-	{ setIdExemplaire(idExemplaire); setDateAchat(dateAchat); setStatus(status); setIsbn(isbn); }	
 	
-	public int getIdExemplaire() {return idExemplaire;}
-	public void setIdExemplaire(int idExemplaire) {this.idExemplaire = idExemplaire;}
-	public GregorianCalendar getDateAchat() {return dateAchat;}
-	public void setDateAchat(GregorianCalendar dateAchat) {this.dateAchat = dateAchat;}
-	public EnumStatusExemplaire getStatus() {return status;}
-	public void setStatus(EnumStatusExemplaire status) {this.status = status;}
-	public String getIsbn() {return isbn;}
-	public void setIsbn(String isbn) {this.isbn = isbn;}
+	public int getIdExemplaire() {
+		return idExemplaire;
+	}
+
+	public void setIdExemplaire(int idExemplaire) {
+		this.idExemplaire = idExemplaire;
+	}
+
+
 	
+	public Date getDateAchat() {
+		return dateAchat;
+	}
+
+	public void setDateAchat(Date dateAchat) {
+		this.dateAchat = dateAchat;
+	}
+
+
+
+	public EnumStatusExemplaire getStatus() {
+		return status;
+	}
+
+	public void setStatus(EnumStatusExemplaire status) {
+		this.status = status;
+	}
+
+
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+
+
+	public enum EnumStatusExemplaire {
+		DISPONIBLE,
+		PRETE,
+		SUPPRIME
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Exemplaire [N° Id Exemplaire = " + idExemplaire + ", Date d'achat = " + sdf.format(dateAchat) + ", Status = " + status
+				+ ", N° ISBN = " + isbn +"]";
+	}
+	
+	
+	public static void main(String[]args) {
+		ExemplairesDAO exd = new ExemplairesDAO();
+		
+		System.out.println("Test ce qu'il y a dans la Dao avant création d'un exemplaire :");
+		for (Exemplaire ex : exd.getExemplaireDB()) {
+			System.out.println(ex.toString());
+		}
+		
+		System.out.println("\nCréation Exemplaire id n° 01");
+		Exemplaire essais = new Exemplaire(01, new GregorianCalendar(2016,10,9).getTime(), EnumStatusExemplaire.DISPONIBLE, "isbn46");
+		
+		System.out.println("\nTest ce qu'il y a dans ExemplaireDB après création d'un exemplaire :");
+		for (Exemplaire ex : exd.getExemplaireDB()) {
+			System.out.println(ex.toString());
+		}
+	}
+
 }

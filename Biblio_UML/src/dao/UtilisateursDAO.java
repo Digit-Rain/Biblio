83f87a0b825b8d00a25b3e6193ff6c2e742f15fb
Digit-Rain <thierrymenard1974@gmail.com>
2017-03-06 14:33:46 +0100

    Add files via upload

--- Biblio_UML/src/dao/UtilisateursDAO.java
+++ Biblio_UML/src/dao/UtilisateursDAO.java
@@ -3,6 +3,9 @@ package dao;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
 
+import metier.Adherent;
+import metier.Employe;
+import metier.EnumCategorieEmploye;
 import metier.Utilisateur;
 
 
@@ -10,13 +13,15 @@ import metier.Utilisateur;
 public class UtilisateursDAO 
 {	
 	
-	
 	// Emulation DAO Utilisateurs
 	private static Utilisateur[] utilisateursDB =
 		{
-			new Utilisateur ( "Francis", "Bacon", new GregorianCalendar(1878,10,5), "H", "maths", "FB", 01),
+			new Adherent ( "Francis", "Bacon", new GregorianCalendar(1878,10,5), "H", "maths", "FB", 01, "01-02-03-04-05"),
 			new Utilisateur ( "Henri", "Bergson", new GregorianCalendar(1900,2,4), "H", "ourson", "HB", 02),
-			new Utilisateur ( "Francis", "Lalanne", new GregorianCalendar(1970,2,5), "H", "tignasse", "FL", 03),	
+			
+			new Employe ( "Francis", "Lalanne", new GregorianCalendar(1970,2,5), 
+						"H", "tignasse", "FL", 03, "Emp01", EnumCategorieEmploye.BIBLIOTHECAIRE),
+			
 		};
 	
 	
@@ -29,6 +34,12 @@ public class UtilisateursDAO
 		{ if ( temp.getIdUtilisateur() == idUtilisateur ) user = temp ;}
 		
 		return user;
+	}
+	
+
+	@Override public String toString() 
+	{
+		return super.toString();
 	}
 	
 	

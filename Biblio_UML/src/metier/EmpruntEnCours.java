c9ba91a5fd93106dd2318a956e3dcd1f803ee42d
davdevo <davdroid67@gmail.com>
2017-03-07 10:42:18 +0100

    EmpruntEnCours.java

--- Biblio_UML/src/metier/EmpruntEnCours.java
+++ Biblio_UML/src/metier/EmpruntEnCours.java
@@ -1,31 +1,74 @@
 package metier;
+import java.text.SimpleDateFormat;
 //import java.util.Date;
 import java.util.GregorianCalendar;
+
+import dao.ExemplairesDAO;
+import dao.UtilisateursDAO;
+import metier.Exemplaire.EnumStatusExemplaire;
 
 
 public class EmpruntEnCours 
 {
-	private GregorianCalendar dateEmprunt = new GregorianCalendar(0,0,0);
-	private int idExemplaire = 0 ;
-	private int idUtilisateur = 0 ;
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	private GregorianCalendar dateEmprunt;
+	private Utilisateur emprunteur;
+	private Exemplaire exemplaire;
+
 	
-	
+	public EmpruntEnCours () { } // constructeur sans param
+	public EmpruntEnCours (Utilisateur lemprunteur, Exemplaire lexemplaire, GregorianCalendar dateEmprunt) throws BiblioException { 
+		this.setEmprunteur(lemprunteur);
+		this.setExemplaire(lexemplaire);
+		this.setDateEmprunt(dateEmprunt);
+		lemprunteur.addEmpruntEnCours(this);
+		}
+
 	public GregorianCalendar getDateEmprunt() {return dateEmprunt;}
-	public int getIdExemplaire() {return idExemplaire;}
-	public int getIdUtilisateur() {return idUtilisateur;}
 	
-	
-	public void setIdExemplaire(int idExemplaire) {this.idExemplaire = idExemplaire;}
-	public void setIdUtilisateur(int idUtilisateur) {this.idUtilisateur = idUtilisateur;}
 	public void setDateEmprunt(GregorianCalendar dateEmprunt) {this.dateEmprunt = dateEmprunt;}
 
-	public EmpruntEnCours () { this(new GregorianCalendar()); }
-	public EmpruntEnCours (GregorianCalendar gregorianCalendar) { setDateEmprunt(gregorianCalendar); }
+	public Utilisateur getEmprunteur() {
+		return emprunteur;
+	}
+	public void setEmprunteur(Utilisateur emprunteur) {
+		this.emprunteur = emprunteur;
+	}
+	public Exemplaire getExemplaire() {
+		return exemplaire;
+	}
+	public void setExemplaire(Exemplaire unExemplaire) {
+		if (unExemplaire.getStatus() == EnumStatusExemplaire.DISPONIBLE) {
+		this.exemplaire = unExemplaire;
+		unExemplaire.setStatus(EnumStatusExemplaire.PRETE);
+		}
+		else
+			try {
+				throw new BiblioException("Emprunt impossible. L'exemplaire n° "+ unExemplaire.getIdExemplaire()+" est déjà prêté.");
+			} catch (BiblioException e) {
+				// TODO Auto-generated catch block
+				System.err.println(e);
+			}
+	}
+	
 
+	@Override
+	public String toString() {
+		return "EmpruntEnCours [Date d'Emprunt = " + sdf.format(dateEmprunt.getTime()) + ", emprunteur = " 
+				+ emprunteur.getPseudonyme() + ", exemplaire = "+ exemplaire.getIdExemplaire() + "]\n";
+	}
 	
-	public static void main(String[] args) 
+	
+	public static void main(String[] args) throws BiblioException 
 	{
-	
+		UtilisateursDAO userDAO = new UtilisateursDAO();
+		ExemplairesDAO exDAO = new ExemplairesDAO();
+		Utilisateur ad1 = userDAO.findByKey(01);
+		Exemplaire ex = exDAO.findByKey(147);
+		
+		EmpruntEnCours ep = new EmpruntEnCours(ad1, ex, new GregorianCalendar());
+		System.out.println("On essaye d'emprunter l'exemplaire n° 147 qui est déjà "+ ex.getStatus()+" => lève BiblioException :");
+		EmpruntEnCours ep2 = new EmpruntEnCours(userDAO.findByKey(02), ex, new GregorianCalendar());
 	}
 
 }
